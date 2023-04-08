10fc830dbda9fff6dad05d0f1d778d56a00b811f
zizo2k21 <yedjoussigue.kone@yahoo.fr>
2023-04-08 13:04:22 +0200

    delete const env

--- database.js
+++ database.js
@@ -5,11 +5,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const pool = mysql.createPool({
-    host:process.env.MYSQL_HOST ? process.env.MYSQL_HOST:"unixshell.hetic.glassworks.tech",
-    port:process.env.MYSQL_PORT ? process.env.MYSQL_PORT:"27116",
-    user:process.env.MYSQL_USER ? process.env.MYSQL_USER:"student",
-    password:process.env.MYSQL_PWD ? process.env.MYSQL_PWD:"Tk0Uc2o2mwqcnIA",
-    database:process.env.MYSQL_DATABASE ? process.env.MYSQL_DATABASE:'sakila'
+    host:process.env.MYSQL_HOST,
+    port:process.env.MYSQL_PORT,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PWD,
+    database:process.env.MYSQL_DATABASE 
 }).promise()
 
 
