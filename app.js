d5d2afc601a73d9139b3b8f128cc6f07e1fb3fea
zizo2k21 <yedjoussigue.kone@yahoo.fr>
2023-04-08 02:09:38 +0200

    push test

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ import { testFunction } from "./database.js";
 
 const app = express()
 
+const port = 5000
+
 
 app.use(cors());
-
+app.get("/", async (req,res)=>{
+    res.send("hello world")
+})
 
 app.get("/test", async (req,res)=>{
     const test = await testFunction(req,res)
@@ -19,6 +23,6 @@ app.use((err, req, res, next)=>{
     res.status(500).send('Something Broke!')
 });
 
-app.listen(5000, ()=>{
-    console.log('server is running on 5000');
+app.listen(process.env.port || 5000, ()=>{
+    console.log(`server is running on ${port}`);
 })
